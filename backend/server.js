7bedf1ff1527f0fa0dd8530720b70d7e18437765
Afif <49422146+Avwaveaf@users.noreply.github.com>
2023-01-14 15:46:23 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:["http://localhost:3000/","https://mern-task-manager-app.onrender.com"]
+    origin:["http://localhost:3000/","https://mern-task-manager-app-j7tz.onrender.com"]
 }));
 app.use("/api/v1/tasks",taskRoutes)
 
